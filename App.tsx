02163e55b1e0052e07bc942827f6ed2d9743492b
modsomjeed <nitcharee.kwang@gmail.com>
2023-02-25 14:38:14 +0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,18 +1,38 @@
 import * as React from 'react';
 import './style.css';
-import Input from './Input';
+// import Input from './Input';
 
 export default function App() {
-  const [input, setInput] = React.useState('');
-
-  const onChange = (v) => {
-    setInput(v);
+  const [state, setState] = React.useState({ firstName: '', lastName: '' });
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(`${state.firstName} : ${state.lastName}`);
   };
 
+  const onChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
   return (
     <React.Fragment>
-      Input: {input}
-      <Input onValueChange={onChange} />
+      <div>test</div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="firstName"
+          value={state.firstName}
+          onChange={onChange}
+          required
+        />
+        <input
+          name="lastName"
+          value={state.lastName}
+          onChange={onChange}
+          required
+        />
+        checkbox radio select
+        <button type="submit">Submit</button>
+      </form>
     </React.Fragment>
   );
 }
