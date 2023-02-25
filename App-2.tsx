1256ac793b184eb6ad339eed047830cf2845c468
modsomjeed <nitcharee.kwang@gmail.com>
2023-02-25 15:18:06 +0700

    1 file modified, 1 added

---
+++ App-2.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import './style.css';
+// import Input from './Input';
+
+export default function App() {
+  const [value, setValue] = React.useState('');
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    console.log('value: ', value);
+    setCount(value.length);
+  });
+
+  return (
+    <div>
+      <div>Value: {value}</div>
+      <div>Count: {count}</div>
+      <input
+        value={value}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+      />
+    </div>
+  );
+}
--- App.tsx
+++ App.tsx
@@ -1,26 +1,47 @@
 import * as React from 'react';
 import './style.css';
-// import Input from './Input';
 
 export default function App() {
-  const [value, setValue] = React.useState('');
-  const [count, setCount] = React.useState(0);
+  const [input, setInput] = React.useState(0);
+  const [resultmod2, setResult2] = React.useState('');
+  const [resultmod10, setResult10] = React.useState('');
 
   React.useEffect(() => {
-    console.log('value: ', value);
-    setCount(value.length);
+    if (input % 2 == 0) {
+      setResult2('odd');
+    } else {
+      setResult2('event');
+    }
+    if (input % 10 == 0) {
+      setResult10('true');
+    } else {
+      setResult10('false');
+    }
   });
 
   return (
-    <div>
-      <div>Value: {value}</div>
-      <div>Count: {count}</div>
-      <input
-        value={value}
-        onChange={(event) => {
-          setValue(event.target.value);
-        }}
-      />
-    </div>
+    <React.Fragment>
+      <div>
+        <div>
+          input: <b>{input}</b>
+        </div>
+        <div>
+          even or odd: <b>{resultmod2}</b>
+        </div>
+        <div>
+          Divisible by 10 : <b>{resultmod10}</b>
+        </div>
+        <form>
+          <input
+            type="number"
+            onChange={(event) => {
+              setInput(Number(event.target.value));
+            }}
+            required
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    </React.Fragment>
   );
 }
